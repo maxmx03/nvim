d2fe31711853d40ea9c4b85db206f3a15f83f748
Max Miliano <milianordelcanto@gmail.com>
2022-07-20 09:09:18 -0300

    remove print

--- init.lua
+++ init.lua
@@ -225,7 +225,6 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
   pattern = { '*.html', '*.css', '*.scss', '*.vue', '*.svelte' },
   once = true,
   callback = function()
-    print('formatoptions')
     vim.cmd 'set formatoptions-=cro'
   end,
 })
