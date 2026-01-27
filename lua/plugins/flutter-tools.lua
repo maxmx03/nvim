ff0b44a2253d05bab97f8acf7e7b7df6603248be
Max Miliano <milianordelcanto@gmail.com>
2026-01-27 11:08:51 -0300

    feat(flutter): shortcut

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -5,6 +5,38 @@ return {
     'nvim-lua/plenary.nvim',
     'stevearc/dressing.nvim',
   },
+  keys = {
+    {
+      '<leader>fo',
+      '<cmd>FlutterOutlineToggle<cr>',
+      desc = 'Flutter Outline',
+    },
+    {
+      '<leader>fr',
+      '<cmd>FlutterRun<cr>',
+      desc = 'Flutter Run',
+    },
+    {
+      '<leader>fR',
+      '<cmd>FlutterRestart<cr>',
+      desc = 'Flutter Restart',
+    },
+    {
+      '<leader>fl',
+      '<cmd>FlutterReload<cr>',
+      desc = 'Flutter Reload',
+    },
+    {
+      '<leader>fe',
+      '<cmd>FlutterDevTools<cr>',
+      desc = 'Flutter Emulators',
+    },
+    {
+      '<leader>fd',
+      '<cmd>FlutterDevices<cr>',
+      desc = 'Flutter Devices',
+    },
+  },
   config = function()
     require('flutter-tools').setup {
       widget_guides = {
