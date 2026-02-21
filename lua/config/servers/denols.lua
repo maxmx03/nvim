a3165dc35467f19686ccdf1a4a197f5e324c2a0e
Max Miliano <milianordelcanto@gmail.com>
2026-02-21 09:19:48 -0300

    refactor(denols): inlayhints

--- lua/config/servers/denols.lua
+++ lua/config/servers/denols.lua
@@ -1,19 +1,41 @@
 return {
   root_markers = { 'deno.json', 'deno.jsonc' },
   settings = {
-    editor = {
+    typescript = {
       inlayHints = {
-        enabled = true,
+        parameterTypes = {
+          enabled = true,
+        },
+        variableTypes = {
+          enabled = true,
+        },
+        functionLikeReturnTypes = {
+          enabled = true,
+        },
       },
     },
     deno = {
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all',
+        },
+        enumMemberValues = { enabled = true },
+      },
       enablePaths = {
         './',
       },
       enable = true,
       lint = true,
       unstable = true,
+      codeLens = {
+        implementations = true,
+        references = true,
+        test = true,
+        referencesAllFunctions = true,
+      },
       suggest = {
+        paths = true,
+        autoImports = true,
         imports = {
           hosts = {
             ['https://deno.land'] = true,
