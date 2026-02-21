7080ac607d8a4b2579b34ec3d45d9b5ebf9f7a24
Max Miliano <milianordelcanto@gmail.com>
2026-02-21 08:55:47 -0300

    fix: denols

--- lua/config/servers/denols.lua
+++ lua/config/servers/denols.lua
@@ -1,7 +1,5 @@
-local nvim_lsp = require 'lspconfig'
-
 return {
-  root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
+  root_markers = { 'deno.json', 'deno.jsonc' },
   settings = {
     editor = {
       inlayHints = {
